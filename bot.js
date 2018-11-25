f9a856c6bcaa327c2b703aae569303fc5f7d621f
DRL7N <40870933+DRL7N@users.noreply.github.com>
2018-11-25 19:42:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,6 +63,6 @@ client.on("message", async message => {
     }
 });
 
-USER.login(rocess.env.TOKENUSER); client.login(rocess.env.TOKENBOT);
+USER.login(process.env.TOKENUSER); client.login(process.env.TOKENBOT);
 
 
